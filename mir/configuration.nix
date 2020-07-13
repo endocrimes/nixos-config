ac41a45226f322cdd5b3af7c931441395510978c
Danielle Lancashire <dani@builds.terrible.systems>
2020-07-13 22:32:48 +0200

    mir: enable ssh-during-boot for entering hdd keys

--- mir/configuration.nix
+++ mir/configuration.nix
@@ -19,6 +19,23 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.initrd.network.ssh = {
+    enable = true;
+    port = 4242;
+    hostKeys = [
+      "/etc/secrets/initrd/ssh_host_rsa_key"
+      "/etc/secrets/initrd/ssh_host_ed25519_key"
+    ];
+    authorizedKeys = [
+      "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAEYESQFFAIJlMTsZvnv9wSjtyHCNfXvlBgvMazJm2jxvH2SHhY3Cn4cDZk2bW/BdyZJHRmXUSDfYoRsGiTlXTluZwFCFg+5wair9y+gxHaTW+903bJDH1EXnkzzAXkf4OEIyudp9UqsCm8KPPJz9zuhGKw1rzq4e2EJwZ3hKAsWB3zsUA=="
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDy6nsIHNmq0zzkXbjutADn2cjOoLjz70+yQPnDku9Da/BdmjQEoArsojI/l5WuP0D2+xUXEOLQonGF1LKdBiBrCn775PVF/wd4MlW1a7uyXiFlYu4a2H8dgaQ79E85/Tpzc9AwzkVb+vq1oii49yQFarc7RHrqXikQ9yDTqWZQ5BYZUSXZVZ+ZCct9Y/3xxQyMD7i1eTaf7t2HfIUusAVzIXfpUfFQ2XxUyoJtRrG2hgTIdUikN0+JDD8Th1d+rPIw+uYNwbrw9qEpMY8MXT4Od8i7/j8Wwyo4iOF04n2nNmV+p1ToQ6iiduZZZ3/npRdhzbgXJK5TNq98R66Igiit"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhAOEkafj40v3nn2ZQJl2ZlRThfFP+H4sxqmEvicEDg"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5Y4MC0iJA0ftL5kkBJwNjp60npCF8Jfjsd7nQzsF8x"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM3bsRW8tLBO3PmpXPrpE635Zu7qOWgWvDRrTm2QQh8Z"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC80KBriR+kXiawNPoBqk8eC6VMXFq7B5QZebqrwoJCTQX7pYrr9wU7WqjjXlYdfPyeZOCbJK/+baBu6OheISt1qfa81C4aKMevU9P4IgtwUN6z44s+xBYhrF/xWrs0CW0SaF+/gM/9nc4Anj9wk7JbFYKqAVbf7KtjtkeS1dUcHo4iIl+Gxx0d3jNoDtkCQBAKpzx4Z4FsA7bD5af3usmfa8YP8eH8VmbNqZY9DXlAr8FZzC7zzRRE7nsshelLTdnve9+wgtE65L6G1k43PsfJXw/kxe2XAb38N2U/ZWw6ZuYvJmF+BKRvPxQhsbie2AT5GH5+AFhQUVPfrZnYwwMWd/F0igc+Iee2nY+knu3OUgeNW50H4yi+/hAkDuyEdyLALg1ttbpWWYlgIhez5k5hY4oi+6H+mRlUV0J431Xk+UwVovzuC8cjjdu0zAWqgTN36DDxiill51Lv5swFi9wUfKoMxMFPA2dZZNmX37h20ftnN2aYSF6B/6Z1ugteB0adsa1lWNdf4/aomWEmvy8GyOQmOIC5KeVsPs5CEKqwLbTiRUYNJDIF7pth1Jq1N6hMCzCEn2qYPMrzXjiHgLAU/ewyT3+wu0FtZGOwEDZj/d1aba26LyxTOJ8H1xb5HHq29StFEfs0OPaXLhVvUSQ9H2jTAj2tTaiVhQ7I02KJGQ=="
+    ];
+  };
+
   networking.hostName = "mir";
   networking.hostId = "6fb90b34";
 
